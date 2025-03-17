0fbc89e85b91df45d05475d137261dae69e3e9d1
gjsoy24 <gour.joy24@gmail.com>
2025-03-17 20:21:30 +0600

    Add image and button with alert to App component

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Alert, Image, Text, TouchableOpacity, View} from 'react-native';
 
 const App = () => {
   return (
@@ -7,8 +7,19 @@ const App = () => {
       <Text>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta neque
         asperiores dolore, adipisci tempora magni nesciunt necessitatibus.
-        Itaque, ad rerum!
+        Itaque, ad rerum!sds
       </Text>
+      <Image
+        style={{width: 200, height: 200}}
+        source={{
+          uri: 'https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',
+        }}
+      />
+      <TouchableOpacity
+        onPress={() => Alert.alert('Hello')}
+        style={{backgroundColor: 'red', padding: 10, borderRadius: 5}}>
+        <Text>Click me</Text>
+      </TouchableOpacity>
     </View>
   );
 };
